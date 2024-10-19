a33a913f1649b85811a24e4e65b038d8f898757d
Sherry Chen <shchen@ad.unc.edu>
2024-10-19 03:43:13 -0400

    upload

---
+++ Expense App/Expense App/Expense_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_AppApp.swift
+//  Expense App
+//
+//  Created by Sherry Chen on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
